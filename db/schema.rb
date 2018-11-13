aa871bbe08cf84d6089d36c8252c340b1cc82fb7
Jessie <jessie.wang@hotmail.co.uk>
2018-11-14 09:28:09 +1100

    rename column name in incidents

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_13_054628) do
+ActiveRecord::Schema.define(version: 2018_11_13_222042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2018_11_13_054628) do
 
   create_table "incidents", force: :cascade do |t|
     t.string "name"
-    t.string "type"
+    t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
