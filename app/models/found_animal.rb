1461a758972a87a32912c47534d7ba1a3c81a90f
Tracy Dean <tracyblackadder@yahoo.com>
2018-11-15 10:02:24 +1100

    start with uploader

--- app/models/found_animal.rb
+++ app/models/found_animal.rb
@@ -1,6 +1,8 @@
 class FoundAnimal < ApplicationRecord
   belongs_to :incident
   belongs_to :user
+
+  mount_uploader :image, ImageUploader
 end
 
 
--- app/models/lost_animal.rb
+++ app/models/lost_animal.rb
@@ -1,4 +1,6 @@
 class LostAnimal < ApplicationRecord
   belongs_to :incident
   belongs_to :user
+
+  mount_uploader :image, ImageUploader
 end
