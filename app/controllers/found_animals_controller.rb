561a49b576665e1b533cb7a8cdda3075477ed39a
jamesrbedwell <jamesrbedwell@gmail.com>
2018-11-14 12:56:59 +1100

    Index, New and Show Page working, haven't yet been able to test Edit

--- app/controllers/found_animals_controller.rb
+++ app/controllers/found_animals_controller.rb
@@ -19,17 +19,16 @@ class FoundAnimalsController < ApplicationController
     animal = FoundAnimal.new
     animal.species = params[:species]
     animal.date_found = params[:date_found]
-    animal.date_posted = params[:date_posted]
     animal.location_found = params[:location_found]
     animal.location_current = params[:location_current]
     animal.image_url = params[:image_url]
     animal.health_status = params[:health_status]
     animal.incident_id = params[:incident_id]
-    animal.user_id = params[:user_id]
+    animal.user_id = 1 #should be session user id
     animal.tags = params[:tags].split(' ')
     animal.claim_status = 'Found'
     if animal.save
-      redirect_to(found_animal_path(animal))
+      redirect_to(found_animals_path)
     else
       render :new
     end
