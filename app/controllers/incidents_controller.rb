6a578192c70724e8c29f139887042f5521c5552e
Andrea Mele <andrea.mele.au@gmail.com>
2018-11-14 15:21:57 +1100

    starti admin privilege feature

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -2,6 +2,13 @@ class IncidentsController < ApplicationController
 
   def index
     @incidents = Incident.all 
+    users = User.all
+    users.each do |user| 
+      if user.user_privilege
+        render layout: 'admin'
+      else
+        render :index
+      end
   end
 
   def show
