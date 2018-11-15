1bf11b3172d5c95fc023b3c1d57b143fb6d1507f
Andrea Mele <andrea.mele.au@gmail.com>
2018-11-15 12:12:39 +1100

    add delete incident admin side

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -1,15 +1,27 @@
 class IncidentsController < ApplicationController
 
   def index
-    @incidents = Incident.all 
-    @current_user = User.find_by(email: params[:email])
-    if @current_user.admin_privilege
-      render layout: 'admin'
+    @incidents = Incident.all
+    if session[:user_id] 
+      @current_user = User.find_by(id: session[:user_id])
+      if @current_user.admin_privilege
+        render layout: 'admin'
+      end
     end
   end
 
   def show
     @incident = Incident.find(params[:id])
+  end
+  
+
+  def destroy
+    incident = Incident.find_by(id: params[:id])
+    if incident.destroy
+      redirect_to('/')
+    else
+      render :index
+    end
   end
 
 end
