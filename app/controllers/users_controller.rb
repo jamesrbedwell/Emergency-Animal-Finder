7d7950df87a099c1b691523a74e13cf729031829
Andrea Mele <andrea.mele.au@gmail.com>
2018-11-16 12:53:29 +1100

    fix registration page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,9 @@ class UsersController < ApplicationController
         user.email = params[:email]
         user.password = params[:password]
         user.save
+        session[:user_id] = user.id
         if user.save
-            redirect_to("/session/#{user.id}")
+            redirect_to '/'
         else
             render :new
         end
