df908b708db999d7bc99e47f3e6bac63e21f3e55
Andrea Mele <andrea.mele.au@gmail.com>
2018-11-19 09:10:26 +1100

    fix link user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,11 +20,16 @@ class UsersController < ApplicationController
 
     def show
         @user = User.find_by(id: params[:id])
-        
         @current_user = User.find_by(id: session[:user_id])
         @animals_lost = LostAnimal.where(user_id: session[:user_id])
         @animals_found = FoundAnimal.where(user_id: session[:user_id])
+
         render :show
+    end
+
+    def profile
+        @user = User.find_by(id: params[:id])
+        render :profile
     end
 
 end
