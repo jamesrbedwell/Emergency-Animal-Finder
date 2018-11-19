52a96d13758ff331ddcd723e1884a22205a1d361
jamesrbedwell <jamesrbedwell@gmail.com>
2018-11-19 13:16:05 +1100

    Added new incident for admins

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -8,8 +8,19 @@ class IncidentsController < ApplicationController
   def show
     @incident = Incident.find(params[:id])
   end
-  
 
+  def create
+    if User.find_by(id: session[:user_id]) == nil
+      redirect_to '/login'
+    else
+      incident = Incident.new
+      incident.name = params[:name]
+      incident.category = params[:category]
+      incident.save
+      redirect_to('/incidents')
+    end
+  end
+  
   def destroy
     incident = Incident.find_by(id: params[:id])
     if incident.destroy
